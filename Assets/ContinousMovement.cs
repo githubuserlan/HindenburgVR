6e37fc3aea8908a4403471f6e95df1bc71041e21
MrMarans <ron_marvin@yahoo.de>
2020-06-07 02:21:19 +0200

    Was übrig geblieben ist von 7 Stunden rumspielen und alles zerstören

---
+++ Assets/ContinousMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ContinousMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode InputSource;
+
+    private XRRig rig;
+    public float gravity = -9.81f;
+    private float fallingSpeed;
+    public float additionalHeight = 0.02f;
+    public LayerMask groundLayer;
+    private Vector2 inputAxis;
+    private CharacterController character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(InputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime* speed);
+
+
+        //gravity
+        bool isGrounded = CheckIfGrounded();
+        if (isGrounded)
+            fallingSpeed = 0;
+        else
+        fallingSpeed += gravity * Time.fixedDeltaTime;
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    bool CheckIfGrounded()
+    {
+        //are we on ground?
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
---
+++ Assets/locomotionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class locomotionController : MonoBehaviour
+{
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThresholt = 0.1f;
+
+    public Rigidbody m_Rigidbody;
+
+    public GameObject Tp_Reticle;
+
+    void Start()
+    {
+        //Physics.IgnoreLayerCollision(0, 10);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftTeleportRay)
+        {
+
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+        if (rightTeleportRay)
+        {
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+        }
+
+    }
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThresholt);
+        return isActivated;
+    }
+}
