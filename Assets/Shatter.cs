0c5606fa83755c73cc4f2d50c2827dcad1734754
MrMarans <ron_marvin@yahoo.de>
2020-06-14 21:29:17 +0200

    Shatter funktioniert
    Shatter funktioniert nun bei Wurf auf Objekte, sowie beim selbst zerstörren

--- Assets/Shatter.cs
+++ Assets/Shatter.cs
@@ -6,29 +6,35 @@ using UnityEngine;
 public class Shatter : MonoBehaviour
 {
     public GameObject destroyedVersion;
-    public float speed;
     public float DestroySpeed;
-    public bool SpeedIsThere;
 
-
+    float speed;
+    bool SpeedIsThere;
+    bool SpawnAndDestroy = false;
     private void Start()
     {
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (SpeedIsThere)
+        if (SpeedIsThere || (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude > DestroySpeed && collision.gameObject.layer == 8/*collision.collider.gameObject.layer == LayerMask.NameToLayer("Grab")*/ ))
         {
-            Instantiate(destroyedVersion, transform.position, transform.rotation);
-            Destroy(gameObject);
+            
+            SpawnAndDestroy = true;   
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         speed = this.GetComponent<Rigidbody>().velocity.magnitude;
-        if (speed>DestroySpeed)
-        { SpeedIsThere = true;
-            Debug.Log("SpeedisThere"); }
+        if (speed > DestroySpeed)
+        { SpeedIsThere = true; }
         else { SpeedIsThere = false; }
+
+        if (SpawnAndDestroy == true)
+        {
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            SpawnAndDestroy = false;
+        }
     }
 }
