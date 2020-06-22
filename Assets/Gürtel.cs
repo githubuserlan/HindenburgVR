60efe1282c6033162c3b7d4d6954a8a6fe33a7c6
MrMarans <ron_marvin@yahoo.de>
2020-06-22 19:42:33 +0200

    Gürtel Inventar

---
+++ Assets/Gürtel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gürtel : MonoBehaviour
+{
+    public Camera Camera;
+    public float heightguertel;
+    public float guertelDelay;
+    // Start is called before the first frame update
+    void Update()
+    {
+        float temp = Camera.gameObject.transform.position.y;
+        this.gameObject.transform.position = Camera.gameObject.transform.position - new Vector3(0,temp/heightguertel, guertelDelay);
+    }
+}
---
+++ Assets/inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class inventory : MonoBehaviour
+{
+
+    public GameObject handObject = null;
+    public XRDirectInteractor interactor = null;
+
+    // 
+    /// <summary>
+    /// Wenn Objekt in Inventar geht, dann soll
+    /// 1. Objekt anderes Material erhalten
+    /// 2. Bei loslassen objekt ins Inventar gehen
+    /// 3. Falls Inventar voll ist, dann soll: UI erscheinen
+    /// 4. Griff ins Inventar holt Objekt wieder raus
+    /// </summary>
+
+    Collider Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider = this.gameObject.GetComponent<Collider>();
+    }
+
+
+
+    private void OnEnable()
+    {
+        interactor.onSelectEnter.AddListener(Grabed);
+        Debug.Log("Grab");
+    }
+    private void OnDisable()
+    {
+     
+    }
+
+    private void Grabed(XRBaseInteractable interactable)
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
