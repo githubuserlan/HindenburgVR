4856be4f7809b19ca013943f42679103c1512342
MrMarans <ron_marvin@yahoo.de>
2020-07-31 22:01:43 +0200

    crash4

--- Assets/AttachTransform.cs
+++ Assets/AttachTransform.cs
@@ -11,8 +11,6 @@ public class AttachTransform : MonoBehaviour
     //public XRBaseInteractable GrabedObject;
     Transform transformPos;
     public GameObject Pivot;
-    public GameObject LeftHand;
-    public GameObject RightHand;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +25,6 @@ public class AttachTransform : MonoBehaviour
         if (ObjectGrabed != null && ObjectGrabed.GetComponent<XRGrabInteractable>()!=null)
         {
             ObjectGrabed.GetComponent<XRGrabInteractable>().attachTransform = Pivot.transform;
-            if(RightHand.gameObject.GetComponent<Inventory>().HitObject == RightHand.gameObject.GetComponent<Inventory>().HitObject && LeftHand.gameObject.GetComponent<Inventory>().gripButtonAction==true)
-            {
-                Pivot = RightHand.gameObject.transform.GetChild(0).gameObject;
-            }
-            else if (RightHand.gameObject.GetComponent<Inventory>().HitObject == RightHand.gameObject.GetComponent<Inventory>().HitObject && RightHand.gameObject.GetComponent<Inventory>().gripButtonAction == true)
-            { Pivot = LeftHand.gameObject.transform.GetChild(0).gameObject; }
-            else { Pivot = this.gameObject.transform.GetChild(0).gameObject; }
-
-
         }
     }
 }
