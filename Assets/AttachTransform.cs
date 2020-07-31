b179ca0e5c799dcf551f8a6e9039ba90d5e4eb5b
MrMarans <ron_marvin@yahoo.de>
2020-07-31 22:35:21 +0200

    Murmelspiel nochmal programmieren, da Unity Crashes und bei Revert alles bis gestern 21:52 Uhr gelöscht worden...

--- Assets/AttachTransform.cs
+++ Assets/AttachTransform.cs
@@ -11,20 +11,36 @@ public class AttachTransform : MonoBehaviour
     //public XRBaseInteractable GrabedObject;
     Transform transformPos;
     public GameObject Pivot;
+    public GameObject LeftHand;
+    public GameObject LeftHandPivot;
+    public GameObject RightHand;
+    public GameObject RightHandPivot;
 
     // Start is called before the first frame update
     void Start()
     {
         Pivot = this.gameObject.transform.GetChild(0).gameObject;
+        LeftHandPivot = LeftHand.transform.GetChild(0).gameObject;
+        RightHandPivot = RightHand.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         ObjectGrabed = this.GetComponent<Inventory>().HitObject;
-        if (ObjectGrabed != null && ObjectGrabed.GetComponent<XRGrabInteractable>()!=null)
+        if (ObjectGrabed != null)
         {
             ObjectGrabed.GetComponent<XRGrabInteractable>().attachTransform = Pivot.transform;
         }
+        if (LeftHand.GetComponent<Inventory>().HitObject == RightHand.GetComponent<Inventory>().HitObject && LeftHand.GetComponent<Inventory>().gripButtonAction == true)
+        {
+            ObjectGrabed.GetComponent<XRGrabInteractable>().attachTransform = RightHandPivot.transform;
+        }
+        else if (RightHand.GetComponent<Inventory>().HitObject == LeftHand.GetComponent<Inventory>().HitObject && RightHand.GetComponent<Inventory>().gripButtonAction == true)
+        {
+            ObjectGrabed.GetComponent<XRGrabInteractable>().attachTransform = LeftHandPivot.transform;
+        }
+
+
     }
 }
